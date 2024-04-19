ff1c72fc161e502261fe131bb330731ccf781818
chava venkat <chavavrajesh1.com>
2024-04-19 14:24:35 +0530

    hello script

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "All variables: $@"
+echo "Number of Variables passed: $#"
+echo "Script Name: $0"
+echo "Current Working Directory: $PWD"
+echo "Home Directory of Current User: $HOME"
+echo ""
+echo ""
