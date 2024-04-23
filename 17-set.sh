ff2a396065be89a991af7d4ce50e3c64d867515f
chava venkat <chavavrajesh1.com>
2024-04-23 18:47:06 +0530

    functions

--- 17-set.sh
+++ 17-set.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-USERID=$(id -u)
+failure(){
+    echo "Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u) #ERR
 
 if [ $USERID -ne 0 ]
 then
@@ -12,7 +18,7 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y
+dnf install mysqll -y
 dnf install git -y
 
 
